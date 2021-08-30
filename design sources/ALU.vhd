b4363d406480db064b3a58ab49c748bda22a7f51
vasra <41552373+vasra@users.noreply.github.com>
2021-08-30 14:57:26 +0300

    Update ALU.vhd

--- design sources/ALU.vhd
+++ design sources/ALU.vhd
@@ -3,12 +3,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity ALU is
-    generic(N : integer := 32);
+    generic(
+        N : integer := 32
+        );
     port(
-        ALUSrc : in std_logic;
-        ALUControl : in std_logic_vector(1 downto 0);
-        SrcA: in std_logic_vector(N - 1 downto 0);
-        SrcB: in std_logic_vector(N - 1 downto 0)
+        ALUSrc     : in std_logic;
+        ALUControl : in std_logic_vector(3 downto 0);
+        SrcA       : in std_logic_vector(N - 1 downto 0);
+        SrcB       : in std_logic_vector(N - 1 downto 0);
+        ALUResult  : out std_logic_vector(N - 1 downto 0);
+        ALUFlags   : out std_logic_vector(3 downto 0)
         );
 end ALU;
 
@@ -16,5 +20,23 @@ architecture Behavioral of ALU is
 
 begin
 
-
+process(SrcA, SrcB, ALUControl) is
+    case(ALUControl) is
+    when "0000" =>
+        ALUResult <= std_logic_vector(signed(SrcA) + signed(SrcB));
+    when "0001" =>
+        ALUResult <= std_logic_vector(signed(SrcA) - signed(SrcB));
+    when "0010" =>
+        if signed(SrcA) > signed(SrcB) then
+            ALUResult <= "00000001";
+        else
+            ALUResult <= "00000000";
+        end if;
+    when "0011" =>
+        ALUResult <= SrcA and SrcB;
+    when "0100" =>
+        ALUResult <= SrcA xor SrcB;
+    end case;
+end process;
+        
 end Behavioral;
